f94b1aa2d8015a6075e07eb7acc84b51ac47b554
Elena-Leonova <lena.postrash@gmail.com>
2023-06-22 21:00:08 +0300

    Project2_Web

--- src/main/java/applications/UserHelper.java
+++ src/main/java/applications/UserHelper.java
@@ -4,7 +4,7 @@ import models.User;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-import javax.jws.soap.SOAPBinding;
+
 
 public class UserHelper extends HelperBase {
     public UserHelper(WebDriver wd) {
